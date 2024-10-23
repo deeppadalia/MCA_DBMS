4b8b72311a9f063ada9748d959c5abbc646ebdc1
Deep Padalia <113300353+deeppadalia@users.noreply.github.com>
2024-10-23 10:07:14 +0530

    Add files via upload

---
+++ LAB19.sql
@@ -0,0 +1,8 @@
+db.STUDENT.insertMany([
+    { RollNo: 101, Name: "Keyur", Birthdate: new Date("1992-01-05"), SPI: 8.5, City: "Rajkot", Backlog: 2, Branch: "CE" },
+    { RollNo: 102, Name: "Hardik", Birthdate: new Date("1994-02-15"), SPI: 9.0, City: "Ahmedabad", Backlog: 0, Branch: "CE" },
+    { RollNo: 103, Name: "Kajal", Birthdate: new Date("1996-03-14"), SPI: 10.00, City: "Baroda", Backlog: 0, Branch: "IT" },
+    { RollNo: 104, Name: "Bhoomi", Birthdate: new Date("1995-06-23"), SPI: 8.90, City: "Ahmedabad", Backlog: 1, Branch: "ICT" },
+    { RollNo: 105, Name: "Harmit", Birthdate: new Date("1994-02-15"), SPI: 9.80, City: "Rajkot", Backlog: 1, Branch: "IT" },
+    { RollNo: 106, Name: "Jay", Birthdate: new Date("1994-02-15"), SPI: 7.9, City: "Rajkot", Backlog: 2, Branch: "CE" }
+]);
